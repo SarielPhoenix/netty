16b37e80e86d0606e604aa70caec4d240eca305c
huqi <huqi@zhxs.com>
2022-08-28 21:44:01 +0800

    first commit

---
+++ src/main/java/com/huqi/demo/testDome/Dome.java
@@ -0,0 +1,17 @@
+package com.huqi.demo.testDome;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @PackageName:com.huqi.demo.testDome
+ * @ClassName:Dome
+ * @Description：
+ * @Date 2022/8/25 21:06
+ */
+@Slf4j
+public class Dome {
+
+    public static void main(String[] args) {
+        log.error("测试 {}", 1);
+    }
+}
